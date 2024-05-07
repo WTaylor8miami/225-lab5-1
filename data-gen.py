fe74718e35a87402f1a90c4d54a827fdeab2589b
MatthewMosinski <91233770+mosinskimatt@users.noreply.github.com>
2024-05-07 12:06:05 -0400

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -11,11 +11,11 @@ def generate_test_data(reviews):
     """Generate test data for the contacts table."""
     db = connect_db()
     for i in range(reviews):
-        ProductName = f'Test Name {i}'
-        Reviewer = f'Test Name {i}'
-        Date = f'0-0-0000{i}'
-        ProductReview = f'Product is great!{i}'
-        db.execute('INSERT INTO reviews (Date, ProductName, Reviewer, ProductReview) VALUES (?, ?, ?, ?)', (Date, ProductName, Reviewer, ProductReview))
+        product_name = f'Test Name {i}'
+        reviewer = f'Test Name {i}'
+        date = f'0-0-0000{i}'
+        product_review = f'Product is great!{i}'
+        db.execute('INSERT INTO reviews (Date, ProductName, Reviewer, ProductReview) VALUES (?, ?, ?, ?)', (date, product_name, reviewer, product_review))
     db.commit()
     print(f'{reviews} test contacts added to the database.')
     db.close()
