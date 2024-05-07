1ab4cd6f473d840de94ea964d201346bd822682d
MatthewMosinski <91233770+mosinskimatt@users.noreply.github.com>
2024-05-07 11:18:04 -0400

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -7,15 +7,17 @@ def connect_db():
     """Connect to the SQLite database."""
     return sqlite3.connect(DATABASE)
 
-def generate_test_data(num_contacts):
+def generate_test_data(reviews):
     """Generate test data for the contacts table."""
     db = connect_db()
-    for i in range(num_contacts):
-        name = f'Test Name {i}'
-        phone = f'123-456-789{i}'
-        db.execute('INSERT INTO contacts (name, phone) VALUES (?, ?)', (name, phone))
+    for i in range(reviews):
+        ProductName = f'Test Name {i}'
+        Reviewer = f'Test Name {i}'
+        Date = f'0-0-0000{i}'
+        ProductReview = f'Product is great!{i}'
+        db.execute('INSERT INTO reviews (Date, ProductName, Reviewer, ProductReview) VALUES (?, ?, ?, ?)', (Date, Product Name, Reviewer, Product Review))
     db.commit()
-    print(f'{num_contacts} test contacts added to the database.')
+    print(f'{reviews} test contacts added to the database.')
     db.close()
 
 if __name__ == '__main__':
