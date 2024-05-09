9e8694b08c1f4d782e26ce82c42f9f4c5b61b012
Wes Taylor <150176347+WTaylor8miami@users.noreply.github.com>
2024-05-08 21:30:24 -0400

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -15,9 +15,9 @@ def generate_test_data(num_Information):
         Address = f'Test Address {i}'
         Date = f'Test Name{i}'
         Email = f'Test Email!{i}'
-        Favorite Food = f'Test Food!{i}'
-        Favorite Drink = f'Test Drink!{i}'
-        db.execute('INSERT INTO Information (Name, Address, Date, Email, Favorite Food, Favorite Drink) VALUES (?, ?, ?, ?, ?, ?)', (Name, Address, Date, Email, Favorite Food, Favorite Drink))
+        Favorite_Food = f'Test Food!{i}'
+        Favorite_Drink = f'Test Drink!{i}'
+        db.execute('INSERT INTO Information (Name, Address, Date, Email, Favorite_Food, Favorite_Drink) VALUES (?, ?, ?, ?, ?, ?)', (Name, Address, Date, Email, Favorite_Food, Favorite_Drink))
     db.commit()
     print(f'{num_Information} test Information added to the database.')
     db.close()
